0b37b7ffae5c5a1175f84dc16d11c3c2855b683e
Yekku <yevgen.liukkonen@gmail.com>
2018-09-13 17:41:13 +0300

    add more task in solution modul

--- src/TheBasicsOfProgramming/solution.js
+++ src/TheBasicsOfProgramming/solution.js
@@ -1,5 +1,6 @@
 import square from './squares';
 import getTriangleArea from './myMathModule';
+import { length, toUpperCase } from '../utils/strings';
 /**
 solution.js
 Рассчитайте площадь поверхности сферы по формуле: S=4πR2.
@@ -86,6 +87,49 @@ export default (side, angle1, angle2) => {
   const sin2 = Math.sin(radians(angle2));
   const sin3 = Math.sin(Math.PI - radians(angle1 + angle2));
   return (1 / 2) * side * side * ((sin1 * sin2) / sin3);
+};
+// END
+*/
+
+/* ========================================================
+Задание
+×
+solution.js
+Реализуйте и экспортируйте по умолчанию функцию,
+которая делает заглавной первую букву каждого слова в предложении.
+
+solution('hello, world!'); // Hello, World!
+Подсказки
+Вычисление длины строки: length(str).
+Перевод строки/буквы в верхний регистр: toUpperCase(str).
+*/
+// BEGIN (write your solution here)
+const solution = (str) => {
+  let result = '';
+  for (let i = 0; i < length(str); i += 1) {
+    if (str[i] === str[0] || str[i - 1] === ' ') {
+      result += toUpperCase(str[i]);
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+};
+// END
+
+console.log(solution('hello, world!')); // Hello, World!
+
+// Решение учителя
+/*
+solution.js
+// BEGIN
+export default (str) => {
+  let result = '';
+  for (let i = 0; i < length(str); i += 1) {
+    const shouldBeBig = str[i] !== ' ' && (i === 0 || str[i - 1] === ' ');
+    result += shouldBeBig ? toUpperCase(str[i]) : str[i];
+  }
+  return result;
 };
 // END
 */
