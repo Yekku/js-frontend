2f7af43c47ce275f1a72e165f1f25a1c77593b2c
Yekku <yevgen.liukkonen@gmail.com>
2018-09-13 12:32:02 +0300

    add check module

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,6 +1,5 @@
 const identity = str => str;
 export const length = str => str.length;
-
 export const reverse = str => str.split('').reverse().join('');
 export const toUpperCase = str => str.toUpperCase();
 
