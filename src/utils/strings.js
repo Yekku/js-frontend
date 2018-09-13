e97a1ec28b5105bea27bf39c2df8871d3177be47
Yekku <yevgen.liukkonen@gmail.com>
2018-09-13 12:19:04 +0300

    add comparers module

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,7 +1,7 @@
 const identity = str => str;
+export const length = str => str.length;
 
 export const reverse = str => str.split('').reverse().join('');
-
-const length = str => str.length;
+export const toUpperCase = str => str.toUpperCase();
 
 export default { identity, length };
